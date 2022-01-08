fbf5159af664a4981313d35005c05f19feadd69d
Bryon Smith <bryon@smithauto.us>
2022-01-08 16:19:47 -0600

    minor change

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { App } from './components/App'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { reducers } from 'reducers'
 
-interface AppProps {
-    color: string
-}
+const store = createStore(reducers, applyMiddleware(thunk))
 
-class App extends React.Component<AppProps> {
-    
-    render() {
-        return (
-            <div>{this.props.color}</div>
-        )
-    }
-}
-
-ReactDOM.render(<App color="red" />, document.getElementById('root'))
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
